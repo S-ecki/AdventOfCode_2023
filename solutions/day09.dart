b5a49919d3508b8a85d02f052568d5617f42cf2b
Simon Eckerstorfer <75510543+S-ecki@users.noreply.github.com>
2023-12-09 18:36:04 +0100

    perf: add faster, iterative solution

--- solutions/day09.dart
+++ solutions/day09.dart
@@ -10,16 +10,20 @@ class Day09 extends GenericDay {
 
   @override
   int solvePart1() {
-    return parseInput().map(extrapolate).sum;
+    return parseInput().map(extrapolateIter).sum;
   }
 
   @override
   int solvePart2() {
-    return parseInput().map((l) => l.reversed).map(extrapolate).sum;
+    return parseInput().map((l) => l.reversed).map(extrapolateIter).sum;
   }
 }
 
-int extrapolate(Iterable<int> line) {
+// My first solution to the problem. I like the cleanlyness and the fact that I
+// managed to write a recursion 😅
+// However, it's very inefficient, both solutions added take roughly 3 mins 🐌
+// Thus, I wrote a second, less clean but much faster solution below.
+int extrapolateRec(Iterable<int> line) {
   // full line of 0 is the base case
   if (line.every((n) => n == 0)) return 0;
 
@@ -29,5 +33,20 @@ int extrapolate(Iterable<int> line) {
   // poisitin easily.
   final diff = zip([line.skip(1), line]).map((zipped) => zipped[0] - zipped[1]);
 
-  return line.last + extrapolate(diff);
+  return line.last + extrapolateRec(diff);
+}
+
+int extrapolateIter(Iterable<int> line) {
+  var sum = 0;
+  var list = line.toList();
+  while (!list.every((element) => element == 0)) {
+    sum += list.last;
+    final diff = <int>[];
+    for (var i = 1; i < list.length; i++) {
+      final diffAt = list.elementAt(i) - list.elementAt(i - 1);
+      diff.add(diffAt);
+    }
+    list = diff;
+  }
+  return sum;
 }
